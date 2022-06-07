1bb435d241e472b6ef9a53de836fa7f216371f09
Eman <aziz.eman@hotmail.com>
2022-06-07 11:22:28 -0400

    Initial dist subtree commit

--- src/components/scripts/getWeather.js
+++ src/components/scripts/getWeather.js
@@ -4,7 +4,7 @@ const apiKey = '8a227b6cb926c6a88967c261a7dca101';
 
 async function getWeather(city) {
     const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
     );
     const weatherData = await response.json();
     displayWeatherInfo(weatherData);
